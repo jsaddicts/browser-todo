a4c39d3d627cd59da7b7057791ffeb2e189a97b5
Manoj Singh Negi <me@manojsinghnegi.com>
2017-07-08 00:24:00 +0530

    fixed v-for running on ul

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -4,20 +4,21 @@
       <div class="row">
         <div class="two-thirds column">
           <input v-on:keyup="onKeyup" class="u-full-width" type="text" placeholder="Add your task" name="task_desc" v-model="task_desc"/>
+          <span class="text-danger" v-if="validationErr">{{validationErr}}</span>
         </div>
         <div class="one-third column">
           <button class="u-full-width" v-on:click="addTask">Add</button>
         </div>
       </div>
     </div>
-    <ul v-for="task in todoList()">
-      <li>
-        <input type="checkbox" v-bind:checked="task.completed" v-on:change="completeTask(task.id)"/>
+    <ul class="task-list">
+      <li v-for="task in todoList()">
+        <input class="task-list-item" type="checkbox" v-bind:checked="task.completed" v-on:change="completeTask(task.id)"/>
         {{task.text}}
       </li>
     </ul>
-    <ul v-for="task in completedList()"> 
-     <li v-on:click="removeTask(task.id)">
+    <ul> 
+     <li v-for="task in completedList()" v-on:click="removeTask(task.id)">
       <del>{{task.text}}</del>
      </li> 
     </ul>
@@ -30,19 +31,33 @@ export default {
   data () {
     return {
       task_list : [],
-      task_desc :''
+      task_desc :'',
+      validationErr: ''
     }
   },
 
   methods: {
 
+    setErr: function (err) {
+      this.validationErr = err;
+    },
+
     addTask : function(){
+      this.setErr('');
+
+      if(!this.task_desc) {
+        this.setErr('Please write a task');
+        return;
+      }
+
       this.task_list.push({
         id : Date.now(),
         text : this.task_desc,
         completed : false
       })
-      this.clear()
+
+      this.clear();  // clear the input box
+
     },
 
     completeTask : function(id){
@@ -57,7 +72,7 @@ export default {
     },
 
     clear : function(){
-      this.task_desc = ""
+      this.task_desc = "";
     },
 
     removeTask : function(id){
@@ -94,11 +109,6 @@ h1, h2 {
 ul {
   list-style-type: none;
   padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
 }
 
 a {
