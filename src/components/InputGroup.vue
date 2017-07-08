095abbba9bd3b76fac60adf18f290f3243926545
Manoj Singh Negi <me@manojsinghnegi.com>
2017-07-08 11:04:51 +0530

    cleaned CSS up

--- src/components/InputGroup.vue
+++ src/components/InputGroup.vue
@@ -51,19 +51,3 @@ export default {
   props: ['label', 'defaultValue']
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-a {
-  color: #42b983;
-}
-</style>
